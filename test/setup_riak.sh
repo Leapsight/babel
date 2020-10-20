7c846ceec4be3459eb78467bc55d77ce8e7d4a44
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-10-20 12:33:38 +0100

    upgraded reliable (big fixes)

--- test/setup_riak.sh
+++ test/setup_riak.sh
@@ -6,3 +6,5 @@ docker exec -it riakkv riak-admin bucket-type create index_collection '{"props":
 docker exec -it riakkv riak-admin bucket-type activate index_collection
 docker exec -it riakkv riak-admin bucket-type create index_data '{"props":{"datatype":"map", "n_val":3, "pw":"quorum", "pr":"quorum", "notfound_ok":false, "basic_quorum":true}}'
 docker exec -it riakkv riak-admin bucket-type activate index_data
+docker exec -it riakkv riak-admin bucket-type create test_map '{"props":{"datatype":"map"}}'
+docker exec -it riakkv riak-admin bucket-type activate test_map
